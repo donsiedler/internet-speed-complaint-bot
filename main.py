2bd3e97e569b4ec7be5714e5d1f28d55c558d7f5
Artur Siedlerski <don.siedler@gmail.com>
2023-02-28 21:27:24 +0100

    Login to Twitter

--- main.py
+++ main.py
@@ -38,7 +38,24 @@ class InternetSpeedTwitterBot:
         print(f"Upload speed: {self.up} Mbps")
 
     def tweet_at_provider(self):
-        pass
+        # Open and sign in to Twitter
+        self.driver.get("https://twitter.com/login/")
+        time.sleep(4)  # Wait for Twitter login page to load
+        username_input = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div['
+                                                            '2]/div/div/div[2]/div[2]/div/div/div/div['
+                                                            '5]/label/div/div[2]/div/input')
+        username_input.send_keys(TWITTER_EMAIL)
+        next_button = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div['
+                                                         '2]/div/div/div[2]/div[2]/div/div/div/div[6]')
+        next_button.click()
+        time.sleep(2)  # Wait for next login page to load
+        password_input = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div['
+                                                            '2]/div/div/div[2]/div[2]/div[1]/div/div/div['
+                                                            '3]/div/label/div/div[2]/div[1]/input')
+        password_input.send_keys(TWITTER_PASSWORD)
+        log_in_button = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div['
+                                                           '2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div')
+        log_in_button.click()
 
 
 chrome_options = Options()
@@ -47,5 +64,5 @@ chrome_options.add_argument("--start-maximized")
 service = Service(ChromeDriverManager().install())
 
 bot = InternetSpeedTwitterBot(options=chrome_options, service=service)
-bot.get_internet_speed()
+# bot.get_internet_speed()
 bot.tweet_at_provider()
